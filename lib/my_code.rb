95b2c14f50b746b3679f754ca16e2fc7eed892ab
Elias Taveras <eliastave12@gmail.com>
2020-03-26 15:23:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -4,11 +4,15 @@ def map(array)
   counter = 0
   while counter < array.length
     new_array.push(yield(array[counter]))
-  counter += 1
+    counter += 1
   end
   new_array
 end
 
 def reduce(array)
-  new_array
+  new_array = []
+  counter = 0
+  while counter < array.length
+    new_array.push(yield(array[counter]))
+    counter
 end
