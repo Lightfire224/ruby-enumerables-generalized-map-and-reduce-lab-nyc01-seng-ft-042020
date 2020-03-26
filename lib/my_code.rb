e4175d71d529294acd5388b684c32cd08c4a7fbc
Elias Taveras <eliastave12@gmail.com>
2020-03-26 15:25:33 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,7 +13,7 @@ def reduce(array)
   new_array = []
   counter = 0
   while counter < array.length
-    new_array.push(yield(array[counter]))
+    running_total += yield(array[counter]))
     counter += 1
   end
   new_array
